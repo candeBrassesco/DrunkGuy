8ba750792de127dc9e03250c2fd2f40c3b16e734
Candela Brassesco <candebrassesco@gmail.com>
2023-04-12 00:56:55 -0300

    Implementación de useParams

--- src/components/ItemDetailContainer/ItemDetailContainer.jsx
+++ src/components/ItemDetailContainer/ItemDetailContainer.jsx
@@ -2,12 +2,15 @@ import { useEffect, useState } from "react"
 import ItemDetail from "../ItemDetail/ItemDetail"
 import { mockFecht } from "../../utils/mockFetch"
 import './ItemDetailContainer.css'
+import { useParams } from "react-router-dom"
 
 const ItemDetailContainer = () => {
     const [product, setProduct] = useState ({})
 
+    const {pid} = useParams()
+
     useEffect(() => {
-        mockFecht('1')
+        mockFecht(pid)
         .then(resp => setProduct(resp))
         .catch(err => console.log(err))
     }, [])
